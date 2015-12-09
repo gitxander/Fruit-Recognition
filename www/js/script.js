1b2c0b8c8b6d1bc2bab0aef5d2e1e2cf3f5eda98
Mark Kirshner Chico <zhacarias.snizer@gmail.com>
2015-12-09 10:37:09 +0800

    string matching

--- www/js/script.js
+++ www/js/script.js
@@ -49,61 +49,60 @@ $(document).on('pagecreate','#home',function(){
       for(var i=0;i<testarray.length;i++){
         sorted.push(testarray[i].toLowerCase());
       }
-      console.log(sorted);
-      if(sorted.indexOf("dragon") > 0 && sorted.indexOf("fruit") > 0){
+      
+      if(sorted.indexOf("dragon") >= 0 && sorted.indexOf("fruit") >= 0){
         //Dragon Fruit
         viewdata("Dragon Fruit","Hylocereus undatus");
       }
-      else if(sorted.indexOf("jackfruit") > 0) {
+      else if(sorted.indexOf("jackfruit") >= 0) {
         viewdata("Jackfruit (Langka)","Artocarpus heterophyllus");
       }
-      else if(sorted.indexOf("pineapple") > 0){
+      else if(sorted.indexOf("pineapple") >= 0){
         viewdata("Pineapple (Pinya)","Ananas comosus");
       }
-      else if(sorted.indexOf("avocado") > 0){
+      else if(sorted.indexOf("avocado") >= 0){
          viewdata("Avocado","Persea americana");
       }
-      else if(sorted.indexOf("banana") > 0){
+      else if(sorted.indexOf("banana") >= 0){
          viewdata("Banana (Saging)","Musa acuminata or Musa balbisiana");
       }
-      else if(sorted.indexOf("sugar") > 0 && sorted.indexOf("apple") > 0){
-        //Dragon Fruit
+      else if(sorted.indexOf("sugar") >= 0 && sorted.indexOf("apple") >= 0){
         viewdata("Sugar Apple (Atis)","Annona squamosa");
       }
-      else if(sorted.indexOf("guava") > 0){
+      else if(sorted.indexOf("guava") >= 0){
          viewdata("Guava (Bayabas)","Psidium guajava");
       }
-      else if(sorted.indexOf("corn") > 0){
+      else if(sorted.indexOf("corn") >= 0){
          viewdata("Sweet Corn (Mais)","Zea mays");
       }
-      else if(sorted.indexOf("mango") > 0){
+      else if(sorted.indexOf("mango") >= 0){
          viewdata("Mango (Manga)","Mangifera indica");
       }
-      else if(sorted.indexOf("coconut") > 0){
+      else if(sorted.indexOf("coconut") >= 0){
          viewdata("Coconut (Buko)","Cocos nucifera");
       }
-      else if(sorted.indexOf("soursop") > 0){
+      else if(sorted.indexOf("soursop") >= 0){
          viewdata("Soursop (Guyabano)","Annona muricata");
       }
-      else if(sorted.indexOf("watermelon") > 0){
+      else if(sorted.indexOf("watermelon") >= 0){
          viewdata("Watermelon (Pakwan)","Citrullus lanatus");
       }
-      else if(sorted.indexOf("rambutan") > 0){
+      else if(sorted.indexOf("rambutan") >= 0){
          viewdata("Rambutan","Nephelium lappaceum");
       }
-      else if(sorted.indexOf("citrus") > 0 || sorted.indexOf("calamondin") > 0){
+      else if(sorted.indexOf("citrus") >= 0 || sorted.indexOf("calamondin") >= 0){
          viewdata("Calamondin (Kalamansi)","Fortunella japonica");
       }
-      else if(sorted.indexOf("citrus") > 0 || sorted.indexOf("Tangerine") > 0){
+      else if(sorted.indexOf("citrus") >= 0 || sorted.indexOf("Tangerine") >= 0){
          viewdata("Tangerine (Dalanghita)","Citrus tangerina");
       }
-      else if(sorted.indexOf("star") > 0 && sorted.indexOf("apple") > 0){
+      else if(sorted.indexOf("star") >= 0 && sorted.indexOf("apple") >= 0){
          viewdata("Star Apple (Kaimito)","Chrysophyllum cainito");
       }
-      else if(sorted.indexOf("mangosteen") > 0){
+      else if(sorted.indexOf("mangosteen") >= 0){
          viewdata("Mangosteen","Garcinia mangostana");
       }
-      else if(sorted.indexOf("citrus") > 0 || sorted.indexOf("pomelo") > 0){
+      else if(sorted.indexOf("citrus") >= 0 || sorted.indexOf("pomelo") >= 0){
          viewdata("Pomelo (Suha)","Citrus maxima");
       }
       else {
